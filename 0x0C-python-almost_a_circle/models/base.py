5110214b1e06096ed3c9ea2120d011a2abfa3c89
simodevjs <simodevjs@gmail.com>
2024-01-15 00:15:49 +0100

    deded

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 class Base:    
     """Base class for the project"""
 
@@ -15,5 +18,34 @@ class Base:
             Base.__nb_objects +=1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            list_dictionaries = []
+        elif type(list_dictionaries) is not list:
+            raise TypeError("list_dictionaries must be a list of dictionaries")
 
+        for d in list_dictionaries:
+            if type(d) is not dict:
+                msg = "list_dictionaries must be a list of dictionaries"
+                raise TypeError(msg)
 
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_obj"""
+        if list_objs is None:
+            list_objs = []
+
+        filename = "{}.json".format(cls.__name__)
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string([o.to_dictionary() for o in list_objs]))
